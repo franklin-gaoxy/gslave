806e0a5b3e950eef5b578defd5f28639245f95ed
gaoxiuyang <gaoxiuyang@aliyun.com>
2024-05-07 16:58:03 +0800

    1

--- backend/mncet/mncet/start.go
+++ backend/mncet/mncet/start.go
@@ -1,6 +1,13 @@
 package mncet
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"mncet/mncet/tools"
+
+	"gopkg.in/yaml.v2"
+)
 
 /*
 这里是cobra启动函数 实际运行代码逻辑的地方
@@ -9,7 +16,20 @@ import "fmt"
 
 // 默认命令执行
 func NewStart(configFilePath string) {
-	fmt.Println(configFilePath)
+	var config tools.Config
+	yamlFile, err := ioutil.ReadFile(configFilePath)
+	if err != nil {
+		log.Fatalf("Error reading YAML file: %s\n", err)
+	}
+
+	// 解析YAML文件内容到结构体
+	err = yaml.Unmarshal(yamlFile, &config)
+	if err != nil {
+		log.Fatalf("Error parsing YAML file: %s\n", err)
+	}
+
+	fmt.Println(config)
+
 }
 
 // init 命令执行
